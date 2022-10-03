639e56e94eb0192554abf7682927dfe044828b84
Ayran <ayran.zip@gmail.com>
2022-10-03 14:55:44 -0300

    Estrutura basica das rotas index, cadastro e error

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route("/<string:name>")
+def error(name):
+    return render_template("error.html", pagina=name)
